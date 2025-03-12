ec9b92595af5843618c21531372985f5ec1e95b4
kdieu4 <hoangthanhdieu2006@gmail.com>
2025-03-12 16:50:15 +0700

    Làm thêm bài 1_1 và bài 1_2

---
+++ Kỹ thuật lập trình/Bài1_1.cpp
@@ -0,0 +1,33 @@
+﻿//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//		int a, b;
+//		cout << "Enter a: "; // nhập a
+//		cin >> a;
+//		cout << "Enter b: "; // nhập b
+//		cin >> b;
+//
+//		long long s; //khai báo s (sum) là tổng
+//		s = a + b; // tính tổng
+//		cout << "Sum: a + b = " << s << "\n"; // xuất tổng đã tính
+//
+//		long long p; //khai báo p (product) là tích, có thể sử dụng kiểu long long khi hai số cần tính lớn 
+//		p = a * b; // tính tích
+//		cout << "Product: a * b = " << p << "\n"; // xuất tích đã tính
+//
+//		int d; // khai báo d (difference) là hiệu
+//		d = a - b;// tính hiệu
+//		cout << "Difference: a - b = " << d << "\n"; // xuất hiệu đã tính
+//
+//		float q; // khai báo q (quotient) là thương 
+//		q = ((float)a / b);// tính thương 
+//		cout << "Quotient: a / b = " << q << "\n"; // xuất thương đã tính
+//
+//		int r; // khai báo r (remainder) là số dư 
+//		r = a % b;// tính số dư
+//		cout << "Remainder: a % b = " << r << "\n"; // xuất số dư đã tính
+//
+//		return 0;
+//}
