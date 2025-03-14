6ad1a9024fc0be28cf80da4b541dd599d7c7e5c6
kdieu4 <hoangthanhdieu2006@gmail.com>
2025-03-14 17:49:22 +0700

    lam bai 2.4

--- Kỹ thuật lập trình/Ex2_3.cpp
+++ Kỹ thuật lập trình/Ex2_3.cpp
@@ -1,27 +1,27 @@
-#include<iostream>
-#include<cmath>
-#include<iomanip> // Thu vien dung dinh dang nhap xuat
-using namespace std;
-
-int main() {
-	//Khai bao va nhap dau vao, ra
-	double x, S;
-	int n;
-	cout << "Enter x: ";
-	cin >> x;
-	cout << "Enter n: ";
-	cin >> n;
-	//2.Xu ly
-	//Neu n chan: Tinh S theo truong hop n chan
-	if (n % 2 == 0) {
-		S = 2016 * x;
-		for (int i = 2; i <= n; i++) {
-			S += pow(x, i) / pow(3, i - 1);
-		}
-	}
-	//Neu n le: Tinh S theo truong hop n le
-	else
-		S = 0;
-	cout << "S = " <<setprecision(5)<< S;
-	return 0;
-}
+//#include<iostream>
+//#include<cmath>
+//#include<iomanip> // Thu vien dung dinh dang nhap xuat
+//using namespace std;
+//
+//int main() {
+//	//Khai bao va nhap dau vao, ra
+//	double x, S;
+//	int n;
+//	cout << "Enter x: ";
+//	cin >> x;
+//	cout << "Enter n: ";
+//	cin >> n;
+//	//2.Xu ly
+//	//Neu n chan: Tinh S theo truong hop n chan
+//	if (n % 2 == 0) {
+//		S = 2016 * x;
+//		for (int i = 2; i <= n; i++) {
+//			S += pow(x, i) / pow(3, i - 1);
+//		}
+//	}
+//	//Neu n le: Tinh S theo truong hop n le
+//	else
+//		S = 0;
+//	cout << "S = " <<setprecision(5)<< S;
+//	return 0;
+//}
---
+++ Kỹ thuật lập trình/Ex2_4.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+bool is_prime(int n) {
+	if (n == 1 || n == 0) {
+		return false;
+	}
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	int n;
+	long long T = 0, D = 0;
+	cout << "Enter n: ";
+	cin >> n;
+	/*bool prime[10005];
+	for (int i = 0; i < 10005; i++) {
+		prime[i] = true;
+	}
+	for (int i = 2; i <= n; i += 1) {
+		for (int j = i * i; j <= n; j += i) {
+			prime[j] = false;
+		}
+	}
+	for (int i = 2; i <= n; i++) {
+		if (prime[i] == true) {
+			cout << i << "\n";
+			T += i;
+			D += 1;
+		}
+	}*/
+	D = n;
+	for (int i = 2; i <= n; i++) {
+		for (int i = 2; i * i <= n; i++) {
+			if (n % i == 0) {
+				T -= i;
+				D -= 1;
+				break;
+			}
+		}
+		T += i;
+	}
+
+	cout << "The sum of prime numbers in range [1, n]: T = " << T << endl;
+	cout << "The number of prime numbers in range [1, n]: D = " << D << endl;
+	return 0;
+}
