de3c25bc3d05d511ace7383844d053ef87fdf279
kdieu4 <hoangthanhdieu2006@gmail.com>
2025-03-14 16:12:15 +0700

    Them ghi chu

--- Kỹ thuật lập trình/Ex1_5.cpp
+++ Kỹ thuật lập trình/Ex1_5.cpp
@@ -2,6 +2,7 @@
 //using namespace std;
 //
 //int main() {
+//	//Khai bao va nhap dau vao toa do cua 3 diem A, B, C
 //	int x1, y1, x2, y2, x3, y3;
 //	cout << "Enter the coordinates of point A: ";
 //	cin >> x1 >> y1;
@@ -9,14 +10,15 @@
 //	cin >> x2 >> y2;
 //	cout << "Enter the coordinates of point C: ";
 //	cin >> x3 >> y3;
+//	//Tinh toa do K
 //	double xK, yK, inter;
 //	xK = (x1 + x2 + x3) / 3;
 //	yK = (y1 + y2 + y3) / 3;
-//
+//	//Tinh Inter
 //	inter = sqrt(pow(x1 - xK, 2) + pow(y1 - yK, 2)) + sqrt(pow(x2 - xK, 2) + pow(y2 - yK, 2)) + sqrt(pow(x3 - xK, 2) + pow(y3 - yK, 2));
-//
-//	cout << "xK = " << xK << endl;
-//	cout << "yK = " << yK << endl;
+//	//IN Inter ra man hinh
+//	/*cout << "xK = " << xK << endl;
+//	cout << "yK = " << yK << endl;*/
 //	cout << "Inter = " << inter;
 //	return 0;
 //}
--- Kỹ thuật lập trình/Ex2_2.cpp
+++ Kỹ thuật lập trình/Ex2_2.cpp
@@ -1,47 +1,47 @@
-#include<iostream>
-using namespace std;
-
-int main() {
-	//Khai bao va nhap dau vao a, b, c
-	int a, b, c;
-	cout << "Nhap a: "; cin >> a;
-	cout << "Nhap b: "; cin >> b;
-	cout << "Nhap c: "; cin >> c;
-
-	//Khai bao delta va dau ra
-	double d, x1, x2;
-	//Xet truong hop phuong trinh khong phai bac 2
-	if (a == 0) {
-		cout << "Phuong trinh khong phai bac 2\n";
-		if (b == 0) {
-			if (c == 0)
-				cout << "Phuong trinh co vo so nghiem\n";
-			else
-				cout << "Phuong trinh vo nghiem";
-		}
-		else {
-			x1 = -1.0 * c / b;
-			cout << "Phuong trinh co mot nghiem duy nhat: x = " << x1 << endl;
-		}
-	}
-	//Truong hop phuong trinh bac 2: Tinh delta va bien luan 
-	else {
-		d = b * b - 4 * a * c;
-		//Neu delta < 0 => Ket luan PT vo nghiem
-		if (d < 0) { 
-			cout << "Phuong trinh vo nghiem";
-		}
-		//Neu delta == 0 => Ket luan PT co nghiem kep. Tinh nghiem
-		else if (d == 0) {
-			x1 = -b * 1.0 / (2 * a);
-			cout << "Phuong trinh co nghiem kep: x = " << x1 << endl;
-		}
-		//Neu delta > 0 => Ket luan PT co nghiem hai nghiem phan biet. Tinh nghiem cua PT
-		else {
-			x1 = 1.0 * (-b + sqrt(d)) / (2 * a);
-			x2 = 1.0 * (-b - sqrt(d)) / (2 * a);
-			cout << "Phuong trinh co hai nghiem phan biet:\n x1 = " << x1 << ", " << "x2 = " << x2 << "\n";
-		}
-	}
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//	//Khai bao va nhap dau vao a, b, c
+//	int a, b, c;
+//	cout << "Nhap a: "; cin >> a;
+//	cout << "Nhap b: "; cin >> b;
+//	cout << "Nhap c: "; cin >> c;
+//
+//	//Khai bao delta va dau ra
+//	double d, x1, x2;
+//	//Xet truong hop phuong trinh khong phai bac 2
+//	if (a == 0) {
+//		cout << "Phuong trinh khong phai bac 2\n";
+//		if (b == 0) {
+//			if (c == 0)
+//				cout << "Phuong trinh co vo so nghiem\n";
+//			else
+//				cout << "Phuong trinh vo nghiem";
+//		}
+//		else {
+//			x1 = -1.0 * c / b;
+//			cout << "Phuong trinh co mot nghiem duy nhat: x = " << x1 << endl;
+//		}
+//	}
+//	//Truong hop phuong trinh bac 2: Tinh delta va bien luan 
+//	else {
+//		d = b * b - 4 * a * c;
+//		//Neu delta < 0 => Ket luan PT vo nghiem
+//		if (d < 0) { 
+//			cout << "Phuong trinh vo nghiem";
+//		}
+//		//Neu delta == 0 => Ket luan PT co nghiem kep. Tinh nghiem
+//		else if (d == 0) {
+//			x1 = -b * 1.0 / (2 * a);
+//			cout << "Phuong trinh co nghiem kep: x = " << x1 << endl;
+//		}
+//		//Neu delta > 0 => Ket luan PT co nghiem hai nghiem phan biet. Tinh nghiem cua PT
+//		else {
+//			x1 = 1.0 * (-b + sqrt(d)) / (2 * a);
+//			x2 = 1.0 * (-b - sqrt(d)) / (2 * a);
+//			cout << "Phuong trinh co hai nghiem phan biet:\n x1 = " << x1 << ", " << "x2 = " << x2 << "\n";
+//		}
+//	}
+//	return 0;
+//}
