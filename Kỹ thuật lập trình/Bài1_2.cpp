53d565147ce115ae145eba54f4e22f9673d8c436
kdieu4 <hoangthanhdieu2006@gmail.com>
2025-03-14 10:13:21 +0700

    Lam bai 3 ko co comment

--- Kỹ thuật lập trình/Bài1_2.cpp
+++ Kỹ thuật lập trình/Bài1_2.cpp
@@ -1,18 +1,18 @@
-﻿#include<iostream>
-using namespace std;
-
-int main() {
-	int n;
-	cout << "Enter number: ";
-	cin >> n;
-	int N, T, C, D;
-	N = n / 1000;
-	n %= 1000;
-	T = n / 100;
-	n %= 100;
-	C = n / 10;
-	n %= 10;
-	D = n;
-	cout << N << " ngan " << T << " tram " << C << " chuc " << D << " don vi";
-	return 0;
-}
+﻿//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//	int n;
+//	cout << "Enter number: ";
+//	cin >> n;
+//	int N, T, C, D;
+//	N = n / 1000;
+//	n %= 1000;
+//	T = n / 100;
+//	n %= 100;
+//	C = n / 10;
+//	n %= 10;
+//	D = n;
+//	cout << N << " ngan " << T << " tram " << C << " chuc " << D << " don vi";
+//	return 0;
+//}
---
+++ Kỹ thuật lập trình/Ex1_3.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	double x;
+	cout << "Enter x: ";
+	cin >> x;
+	long double ans;
+	ans = pow(x, 2) + exp(fabs(x)) + pow(sin(x), 2);
+	ans = ans / pow(pow(x, 2) + 1, 1.0 / 5);
+	cout << "Answer: " << ans;
+	return 0;
+}
