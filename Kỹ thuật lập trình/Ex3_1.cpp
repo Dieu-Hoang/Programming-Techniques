001d33d638e46a10b5b14b4e62e054844161bf35
kdieu4 <hoangthanhdieu2006@gmail.com>
2025-04-14 08:39:56 +0700

    Bai tap 3.1 ky thuat lap trinh voi module

---
+++ Kỹ thuật lập trình/Ex3_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+// #include <iomanip> // thu vien dung ham setprecision
+using namespace std;
+
+long long GT(int n) {
+	long long Gt = 1;
+	if (n == 0 || n == 1) return 1;
+	for (int i = n; i >= 1; i--) {
+		Gt = Gt * i;
+	}
+	return Gt;
+	//return n * GT(n - 1);
+}
+
+int main() {
+	int n;
+	double S;
+	cout << "Nhap so nguyen duong n: ";
+	cin >> n;
+	S = (double) (GT(n) + 1) / GT(n + 1); 
+	cout << "S = " << S << endl;
+	return 0;
+}
---
+++ Kỹ thuật lập trình/hello.cpp
@@ -0,0 +1,18 @@
+//#include<iostream>
+//#inluud
+//using namespace std;
+//
+//int main() {
+//	int a, b, c, chuvi, dientich;
+//	cout << "Nhap do dai 3 canh cua tam giac: ";
+//	cin >> a >> b >> c;
+//	if (a + b >= c || b + c >= a || c + a >= b) {
+//		cout << "3 doan thang tao thanh tam giac";
+//		chuvi = a + b + c;
+//		cout << "\nChu vi cua tam giac: " << chuvi;
+//		dientich = sqrt()
+//	}
+//	else
+//		cout << "3 doan thang khong tao thanh tam giac";
+//	return 0;
+//}
