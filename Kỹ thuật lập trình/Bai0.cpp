a75ed3557d07e8b53ec8a91b8cedd63774836bb6
kdieu4 <hoangthanhdieu2006@gmail.com>
2025-06-09 16:02:57 +0700

    ghi vao file

--- Kỹ thuật lập trình/Bai0.cpp
+++ Kỹ thuật lập trình/Bai0.cpp
@@ -8,6 +8,7 @@ Viết chương trình thực hiện các yêu cầu sau.
 Chèn số thực x vào vị trí thứ k trong dãy số nếu k ≤ n, ngược lại chèn x vào vị trí thứ n+1.
 */
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -57,15 +58,42 @@ int main() {
 	int n;
 	nhapSoNguyen(n);
 	float* head, * p;
-	head = new float[n + 2];
+	head = new float[n + 1];
 	p = head;
 	nhapMang(p, n);
 	cout << "Mang da nhap: ";
 	hienMang(p, n);
+
+	ofstream outFile1("Bai0.txt", ios::out);
+	if (outFile1.is_open()) {
+		outFile1 << "Mang da nhap: ";
+		for (int i = 0; i < n; i++) {
+			outFile1 << *(p + i) << " ";
+		}
+		outFile1 << endl;
+		outFile1.close();
+	}
+	else {
+		cout << "Khong the mo file de ghi!" << endl;
+	}
 	
 	chenMang(p, n);
 	cout << "Mang sau khi chen: ";
 	hienMang(p, n);
+
+	ofstream outFile2("Bai0.txt", ios::app);
+	if (outFile2.is_open()) {
+		outFile2 << "Mang sau khi chen: ";
+		for (int i = 0; i < n; i++) {
+			outFile2 << *(p + i) << " ";
+		}
+		outFile2 << endl;
+		outFile2.close();
+	}
+	else {
+		cout << "Khong the mo file de ghi!" << endl;
+	}
+
 
 	delete[] head; 
 	return 0;
